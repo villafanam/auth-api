8e5fbb11a9b8a1aafaa08f9d270973ff723dd9ca
Marco <pastry07@gmail.com>
2023-02-23 13:37:45 -0800

    added more test

--- __test__/v1.test.js
+++ __test__/v1.test.js
@@ -1,6 +1,8 @@
+'use strict';
+
 const { server } = require('../src/server');
-const supertest = require('supertest');
 const { db } = require('../src/models');
+const supertest = require('supertest');
 const request = supertest(server);
 
 beforeAll(async () => {
@@ -12,10 +14,10 @@ afterAll(async () => {
 });
 
 describe('v1 Routes', () => {
-  it('creates a record', async () => {
+  it('creates a record', async() => {
     let response = await request.post('/api/v1/food').send({
       name: 'tacos',
-      calories: '100',
+      calories: 100,
       type: 'protein',
     });
 
@@ -23,5 +25,37 @@ describe('v1 Routes', () => {
     expect(response.body.name).toEqual('tacos');
   });
 
-  
-});
+  it('gets all records', async() => {
+    let response = await request.get('/api/v1/food');
+
+    expect(response.status).toEqual(200);
+    expect(response.body[0].name).toEqual('tacos');
+  });
+
+  it('gets a single records', async() => {
+    let response = await request.get('/api/v1/food/1');
+
+    expect(response.status).toEqual(200);
+    expect(response.body.name).toEqual('tacos');
+  });
+
+  it('updates a record', async() => {
+    let response = await request.put('/api/v1/food/1').send({
+      name: 'tacos',
+      calories: 1000,
+      type: 'protein',
+    });
+
+    expect(response.status).toEqual(200);
+    expect(response.body.name).toEqual('tacos');
+    expect(response.body.calories).toEqual(1000);
+  });
+
+  it('deletes a record', async() => {
+    let response = await request.delete('/api/v1/food/1');
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(1);
+  });
+
+}); 
